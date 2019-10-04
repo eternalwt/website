7f9ffb71b7eaa048606e6c15fd70c434fa5d56ae
eternalwt <eternalwt@qq.om>
2019-10-04 22:33:55 +0800

    001

--- src/main/java/com/greengiant/website/controller/UserController.java
+++ src/main/java/com/greengiant/website/controller/UserController.java
@@ -35,6 +35,23 @@ public class UserController{
         return ResultUtils.success();
     }
 
+    @PostMapping(value = "/password/change")
+    public ResultBean changePassword(@RequestBody AddUserVo userVo) {
+        // todo 写好
+        //1.判断是否有同名的已存在，如果有给出fail的返回值
+        // todo 判空
+        User user = userService.getByName(userVo.getUserName());
+        if (user != null) {
+            return ResultUtils.fail(StatusCodeEnum.USER_EXISTS.getCode(), StatusCodeEnum.USER_EXISTS.getMsg());
+        }
+        //2.分别在user和role关联表里面写数据
+        userService.addUser(userVo);
+
+        //todo 看看异常在哪一层怎么处理，manager？事务放在哪一层的问题
+
+        return ResultUtils.success();
+    }
+
     @GetMapping(value = "/getMessage")
     public String getMessage() {
         return "您拥有用户权限，可以获得该接口的信息！";

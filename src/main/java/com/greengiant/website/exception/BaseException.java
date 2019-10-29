d463263ba1c15291c8d251543d46883d5d443e89
eternalwt <eternalwt@qq.om>
2019-10-29 09:46:23 +0800

    add BaseException

--- src/main/java/com/greengiant/website/exception/BaseException.java
+++ src/main/java/com/greengiant/website/exception/BaseException.java
@@ -1,4 +1,20 @@
 package com.greengiant.website.exception;
 
-public class BaseException {
+public class BaseException extends Exception {
+    // todo 公司里面的代码加了 serialVersionUID 和 isPropertiesKey，有什么用？
+    private int errorCode;
+
+    public BaseException(int errorCode, String message) {
+        super(message);
+        this.setErrorCode(errorCode);
+    }
+
+    public int getErrorCode() {
+        return errorCode;
+    }
+
+    public void setErrorCode(int errorCode) {
+        this.errorCode = errorCode;
+    }
+
 }

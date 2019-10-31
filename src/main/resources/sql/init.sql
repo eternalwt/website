d420709a6d69747bee774301257c1204eaf9d3d1
eternalwt <eternalwt@qq.om>
2019-10-31 19:51:31 +0800

    add menu table

--- src/main/resources/sql/init.sql
+++ src/main/resources/sql/init.sql
@@ -11,7 +11,7 @@ create table auth_user(
   password_salt varchar(128),
   create_time timestamp DEFAULT CURRENT_TIMESTAMP,
   update_time timestamp DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
-) ENGINE=InnoDB;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COMMENT='用户（账号）表';
 
 drop table if exists auth_role;
 create table auth_role(
@@ -20,7 +20,7 @@ create table auth_role(
     description varchar(256),
     create_time timestamp DEFAULT CURRENT_TIMESTAMP,
     update_time timestamp DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
-) ENGINE=InnoDB;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COMMENT='角色表';
 
 drop table if exists auth_permission;
 create table auth_permission(
@@ -29,7 +29,7 @@ create table auth_permission(
     description varchar(256),
     create_time timestamp DEFAULT CURRENT_TIMESTAMP,
     update_time timestamp DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
-) ENGINE=InnoDB;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COMMENT='权限表';
 
 drop table if exists auth_user_role;
 create table auth_user_role(
@@ -38,7 +38,7 @@ create table auth_user_role(
   role_id bigint not null,
   create_time timestamp DEFAULT CURRENT_TIMESTAMP,
   update_time timestamp DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
-) ENGINE=InnoDB;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COMMENT='用户角色关联表';
 
 drop table if exists auth_role_permission;
 create table auth_role_permission(
@@ -47,7 +47,25 @@ create table auth_role_permission(
   permission_id bigint not null,
   create_time timestamp DEFAULT CURRENT_TIMESTAMP,
   update_time timestamp DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
-) ENGINE=InnoDB;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COMMENT='角色权限关联表';
 
 insert into auth_user(user_name, password)values('zhang','123');
 -- insert into shiro_user_role(username, role_name) values('zhang', 'admin');
+
+drop table if exists menu;
+create table menu(
+     id bigint auto_increment primary key,
+     menu_name varchar(64),
+     number varchar(64), -- 编号
+     url varchar(128),
+     icon varchar(256),
+     parent_id bigint,
+     sort int,
+     in_use tinyint,
+     open_way tinyint,
+     role varchar(256),
+     permission varchar(512),
+     permission_id bigint not null,
+     create_time timestamp DEFAULT CURRENT_TIMESTAMP,
+     update_time timestamp DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COMMENT='菜单表';

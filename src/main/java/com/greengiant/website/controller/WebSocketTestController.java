c13e1edb58bdc963b4fb9f3f8a2922d93ea67554
eternalwt <eternalwt@qq.om>
2019-11-20 23:14:28 +0800

    add websocket code

---
+++ src/main/java/com/greengiant/website/controller/WebSocketTestController.java
@@ -0,0 +1,27 @@
+package com.greengiant.website.controller;
+
+import com.greengiant.website.pojo.ResultBean;
+import com.greengiant.website.utils.ResultUtils;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.io.IOException;
+
+@RestController
+@RequestMapping("/ws")
+public class WebSocketTestController {
+
+    @RequestMapping(value = "/send", method = {RequestMethod.GET})
+    public ResultBean send(@RequestParam String message, @RequestParam String type) {
+        try {
+            WebSocketServer.sendInfo(message, type);
+        } catch (IOException ex) {
+            // todo
+        }
+
+        return ResultUtils.success("success");
+    }
+
+}
--- src/main/java/com/greengiant/website/websocket/WebSocketController.java
+++ src/main/java/com/greengiant/website/websocket/WebSocketController.java
@@ -5,12 +5,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.handler.annotation.SendTo;
 import org.springframework.messaging.simp.SimpMessagingTemplate;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.util.HtmlUtils;
 
 @Slf4j
-@Controller
+// @Controller
 public class WebSocketController {
 
     @MessageMapping("/ws/hello")

124e4a5dffeb63f59e943ac805bba17b8bfb75c3
eternalwt <eternalwt@qq.om>
2020-02-14 16:35:21 +0800

    add search param

--- src/main/java/com/greengiant/website/controller/ArticleController.java
+++ src/main/java/com/greengiant/website/controller/ArticleController.java
@@ -6,10 +6,17 @@ import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import com.greengiant.website.pojo.PageParam;
 import com.greengiant.website.pojo.ResultBean;
 import com.greengiant.website.pojo.model.Article;
+import com.greengiant.website.pojo.vo.PageQuery;
 import com.greengiant.website.service.ArticleService;
 import com.greengiant.website.utils.ResultUtils;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.LinkedHashMap;
+import java.util.Map;
 
 @RestController
 @RequestMapping("/article")
@@ -23,10 +30,24 @@ public class ArticleController {
         return ResultUtils.success(articleService.save(article));
     }
 
+//    @PostMapping(value = "list")
+//    public ResultBean getPageList(@RequestBody PageParam pageParam) {// todo 分页加条件过滤，形成一个通用的操作
+//        IPage<Article> page = new Page<>(pageParam.getPageNum(), pageParam.getPageSize());
+//        QueryWrapper<Article> wrapper = new QueryWrapper<>();// todo 分页加条件过滤，形成一个通用的操作
+//        IPage<Article> result = articleService.page(page, wrapper);
+//
+//        return ResultUtils.success(result);
+//    }
+
     @PostMapping(value = "list")
-    public ResultBean getPageList(@RequestBody PageParam pageParam) {// todo 分页加条件过滤，形成一个通用的操作
+    public ResultBean getPageList(@RequestBody PageQuery pageQuery) {
+        // todo 判空的问题？
+        Map conditionMap = (LinkedHashMap)pageQuery.getCondition();
+        PageParam pageParam = pageQuery.getPageParam();
+
         IPage<Article> page = new Page<>(pageParam.getPageNum(), pageParam.getPageSize());
         QueryWrapper<Article> wrapper = new QueryWrapper<>();// todo 分页加条件过滤，形成一个通用的操作
+        wrapper.allEq(conditionMap);
         IPage<Article> result = articleService.page(page, wrapper);
 
         return ResultUtils.success(result);
---
+++ src/main/java/com/greengiant/website/pojo/vo/PageQuery.java
@@ -0,0 +1,40 @@
+package com.greengiant.website.pojo.vo;
+
+import com.greengiant.website.pojo.PageParam;
+
+public class PageQuery {
+    private Object condition;
+
+    private PageParam pageParam;
+
+    public Object getCondition() {
+        return condition;
+    }
+
+    public void setCondition(Object condition) {
+        this.condition = condition;
+    }
+
+    public PageParam getPageParam() {
+        return pageParam;
+    }
+
+    public void setPageParam(PageParam pageParam) {
+        this.pageParam = pageParam;
+    }
+
+    public PageQuery() {}
+
+    public PageQuery(Object condition, PageParam pageParam) {
+        this.condition = condition;
+        this.pageParam = pageParam;
+    }
+
+    @Override
+    public String toString() {
+        return "PageQuery{" +
+                "condition=" + condition +
+                ", pageParam=" + pageParam +
+                '}';
+    }
+}

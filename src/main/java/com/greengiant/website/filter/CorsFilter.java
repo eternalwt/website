98fc43ef033d78ade1bebf89a5298077851ee31a
eternalwt <eternalwt@qq.om>
2020-02-20 14:59:46 +0800

    cors config

--- src/main/java/com/greengiant/website/filter/CorsFilter.java
+++ src/main/java/com/greengiant/website/filter/CorsFilter.java
@@ -1,6 +1,7 @@
 package com.greengiant.website.filter;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 
 import javax.servlet.*;
 import javax.servlet.annotation.WebFilter;
@@ -15,12 +16,24 @@ public class CorsFilter implements Filter {
     public void doFilter(ServletRequest req, ServletResponse res, FilterChain chain) throws IOException, ServletException {
         HttpServletResponse response = (HttpServletResponse) res;
         HttpServletRequest request = (HttpServletRequest) req;
-//        response.setHeader("Access-Control-Allow-Origin","*");
+
+        //        response.setHeader("Access-Control-Allow-Origin","*");
         response.setHeader("Access-Control-Allow-Origin", request.getHeader("origin"));
         response.setHeader("Access-Control-Allow-Credentials", "true");
         response.setHeader("Access-Control-Allow-Methods", "POST, GET, PATCH, DELETE, PUT, OPTIONS");
+//        response.setHeader("Access-Control-Allow-Methods", "*");
         response.setHeader("Access-Control-Max-Age", "3600");
         response.setHeader("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+//        response.setHeader("Access-Control-Allow-Headers", "**");// todo 这样配通配符无效，再找一下
+
+        // 预请求 options 直接返回
+        // todo 下面是为了解决：Response to preflight request doesn't pass access control check: It does not have HTTP ok status.
+        // todo 再深入一下：https://www.jianshu.com/p/09781abe53e8
+        if (request.getMethod().equalsIgnoreCase(HttpMethod.OPTIONS.name())) {
+            response.setStatus(HttpServletResponse.SC_OK);
+            return;
+        }
+
         chain.doFilter(req, res);
     }
 }

028983aebd4539a6c929cdee7ef8409cd8bbb95d
unknown <eternalwt@qq.com>
2020-08-02 13:18:26 +0800

    refactor code

--- src/main/java/com/greengiant/website/shiro/RetryLimitHashedCredentialsMatcher.java
+++ src/main/java/com/greengiant/website/shiro/RetryLimitHashedCredentialsMatcher.java
@@ -21,15 +21,15 @@ public class RetryLimitHashedCredentialsMatcher extends HashedCredentialsMatcher
 
     private static final String cacheName = "passwordRetryCache";
 
-//    CacheManager cacheManager = new CacheManager();// todo 能否作为参数传入？
-    @Autowired
+    @Autowired // todo 怎么理顺这个告警很重要
     // todo 在现在的配置下，这里还是用的ehcache。注释掉ehcache后把redis版的写通
+    // todo 能否作为参数传入？
     private CacheManager cacheManager; // todo 类上面没有注解，成员也是可以autowired的。1.搞懂扫描过程；2.这样会不会导致代码混乱不好维护？
 
     @Override
     public boolean doCredentialsMatch(AuthenticationToken token, AuthenticationInfo info) {
         String username = (String)token.getPrincipal();
-        if(this.getItem(username) != null && Integer.valueOf(this.getItem(username).toString()) >= MAX_RETRY_COUNT) {
+        if(this.getItem(username) != null && Integer.parseInt(this.getItem(username).toString()) >= MAX_RETRY_COUNT) {
             // todo 重试的时候需要输入验证码
             throw new ExcessiveAttemptsException("您已连续输错" + MAX_RETRY_COUNT + "次密码！请30分钟后再试");// todo 30根据配置来写
         }
@@ -41,11 +41,11 @@ public class RetryLimitHashedCredentialsMatcher extends HashedCredentialsMatcher
         }
         else {
             if(this.getItem(username) == null) {
-                this.putItem(username, 0);
+                this.putItem(username, 1);
                 throw new IncorrectCredentialsException("密码错误");
             }
             else {
-                this.updateItem(username, Integer.valueOf(this.getItem(username).toString()) + 1);
+                this.updateItem(username, Integer.parseInt(this.getItem(username).toString()) + 1);
                 throw new IncorrectCredentialsException("密码错误，已连续错误" + (this.getItem(username).toString())
                         + "次，最多连续错误" + MAX_RETRY_COUNT + "次");
             }

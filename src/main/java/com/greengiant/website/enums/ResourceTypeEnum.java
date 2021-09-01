850187558984b428ae369d01188a0ded517f4ec8
unknown <eternalwt@qq.com>
2021-09-01 23:11:10 +0800

    add enum

---
+++ src/main/java/com/greengiant/website/enums/ResourceTypeEnum.java
@@ -0,0 +1,44 @@
+package com.greengiant.website.enums;
+
+public enum ResourceTypeEnum {
+
+    MENU(1, "菜单"),
+    BUTTON(2, "按钮");
+
+    private int code;
+
+    private String name;
+
+    ResourceTypeEnum(int code, String name) {
+        this.code = code;
+        this.name = name;
+    }
+
+    public static String getName(int code) {
+        for (ResourceTypeEnum type : ResourceTypeEnum.values()) {
+            if (type.getCode() == code) {
+                return type.getName();
+            }
+        }
+
+        return null;
+    }
+
+    public int getCode() {
+        return code;
+    }
+
+    public void setCode(int code) {
+        this.code = code;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+
+}

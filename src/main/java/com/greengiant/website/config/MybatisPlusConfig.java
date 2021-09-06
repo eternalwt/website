bc1eec358528e3e9e35e7f028425c888748fde6d
unknown <eternalwt@qq.com>
2021-09-06 20:54:09 +0800

    upgrade mybatis-plus

--- src/main/java/com/greengiant/website/config/MybatisPlusConfig.java
+++ src/main/java/com/greengiant/website/config/MybatisPlusConfig.java
@@ -1,6 +1,7 @@
 package com.greengiant.website.config;
 
-import com.baomidou.mybatisplus.extension.plugins.PaginationInterceptor;
+//import com.baomidou.mybatisplus.extension.plugins.PaginationInterceptor;
+import com.baomidou.mybatisplus.extension.plugins.inner.PaginationInnerInterceptor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
@@ -13,9 +14,14 @@ public class MybatisPlusConfig {
     /**
      * 分页插件
      */
+//    @Bean
+//    public PaginationInterceptor paginationInterceptor() {
+//        return new PaginationInterceptor();
+//    }
+
     @Bean
-    public PaginationInterceptor paginationInterceptor() {
-        return new PaginationInterceptor();
+    public PaginationInnerInterceptor paginationInterceptor() {
+        return new PaginationInnerInterceptor();
     }
 
 //    /**
